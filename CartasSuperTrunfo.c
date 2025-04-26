77217df6d532c01abac8f7400320ef6819be64f9
gabrielesguimaraes <gabrielesoaresbs@gmail.com>
2025-04-26 01:54:01 +0000

    Inseri o código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,8 +2,67 @@
 
 int main() {
 
-    printf("Desafio Super Trunfo\n");
-    printf("novo commit\n");
+    #include <stdio.h>
 
-    return 0;
-}
+    int main(){
+    
+        char Estado_1, Estado_2;
+        char Codigo_1, Codigo_2 [50];
+        char Cidade_1, Cidade_2[50];
+        int Populacao_1, Populacao_2;
+        float Area_1, Area_2;
+        float PIB_1, PIB_2;
+        int Turismo_1, Turismo_2;
+
+        printf("Desafio Super Trunfo\n");
+    
+        printf("Digite a primeira letra de 'A' a 'H' (representando um dos oito estados)\n");
+        scanf("%c", &Estado_1);
+    
+        printf("Digite a letra do estado seguida de um número de 01 a 04 (ex: A01, B03)\n");
+        scanf("%s", &Codigo_1);
+    
+        printf("Digite o nome da primeira cidade\n");
+        scanf("%s", &Cidade_1);
+    
+        printf("Digite o número de habitantes da primeira cidade\n");
+        scanf("%d", &Populacao_1);
+    
+        printf("Digite o tamanho da área da primeira cidade em quilômetros quadrados\n");
+        scanf("%f", &Area_1);
+    
+        printf("Digite o PIB da primeira cidade\n");
+        scanf("%f", &PIB_1);
+    
+        printf("Digite a quantidade de pontos turísticos da primeira cidade\n");
+        scanf("%d", &Turismo_1);
+    
+        printf("Digite a segunda letra de 'A' a 'H' (representando um dos oito estados)\n");
+        scanf(" %c", &Estado_2);
+    
+        printf("Digite a letra do estado seguida de um número de 01 a 04 (ex: A01, B03)\n");
+        scanf("%s", &Codigo_2);
+    
+        printf("Digite o nome da segunda cidade\n");
+        scanf("%s", &Cidade_2);
+    
+        printf("Digite o número de habitantes da segunda cidade\n");
+        scanf("%d", &Populacao_2);
+    
+        printf("Digite o tamanho da área da segunda cidade em quilômetros quadrados\n");
+        scanf("%f", &Area_2);
+    
+        printf("Digite o PIB da segunda cidade\n");
+        scanf("%f", &PIB_2);
+    
+        printf("Digite a quantidade de pontos turísticos da segunda cidade\n");
+        scanf("%d", &Turismo_2);
+    
+        printf("Respostas capturadas. Obrigado!\n");
+    
+        return 0;
+        
+    }
+
+
+  
